e66644cf86f03a660f12d5bdcd1fba4803862be1
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-08-25 11:44:17 +0700

    chore: cleanup initial project

--- src/main/java/com/tonghoangvu/r2sfrontendinternship/R2sFrontendInternshipApplication.java
+++ src/main/java/com/tonghoangvu/r2sfrontendinternship/R2sFrontendInternshipApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class R2sFrontendInternshipApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(R2sFrontendInternshipApplication.class, args);
 	}
-
 }
--- src/test/java/com/tonghoangvu/r2sfrontendinternship/R2sFrontendInternshipApplicationTests.java
+++ src/test/java/com/tonghoangvu/r2sfrontendinternship/R2sFrontendInternshipApplicationTests.java
@@ -5,9 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class R2sFrontendInternshipApplicationTests {
-
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }
