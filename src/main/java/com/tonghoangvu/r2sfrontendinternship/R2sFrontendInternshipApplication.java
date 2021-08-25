cad9a1002d0c60a02b265e3378312b59b956e4e3
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-08-25 11:39:33 +0700

    feat: init project

---
+++ src/main/java/com/tonghoangvu/r2sfrontendinternship/R2sFrontendInternshipApplication.java
@@ -0,0 +1,13 @@
+package com.tonghoangvu.r2sfrontendinternship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2sFrontendInternshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2sFrontendInternshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonghoangvu/r2sfrontendinternship/R2sFrontendInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonghoangvu.r2sfrontendinternship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2sFrontendInternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
