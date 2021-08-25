6559aa5bd31c1edb9539cf4b6cb99782768a851d
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-08-25 17:48:57 +0700

    feat: add basic form login

---
+++ src/main/java/com/tonghoangvu/r2sfrontendinternship/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.tonghoangvu.r2sfrontendinternship.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("email@example.com")
+			.password(passwordEncoder().encode("123456"))
+			.roles("USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/").permitAll()
+			.antMatchers("/login.html").permitAll()
+			.antMatchers("/register.html").permitAll()
+			.antMatchers("/css/**").permitAll()
+			.antMatchers("/js/**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin()
+			.loginPage("/login.html")
+			.usernameParameter("email")
+			.loginProcessingUrl("/login")
+			.defaultSuccessUrl("/edit-profile.html", true)
+			.failureUrl("/login.html?error=true")
+			.and()
+			.logout()
+			.logoutUrl("/logout")
+			.deleteCookies("JSESSIONID")
+			.logoutSuccessUrl("/");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
