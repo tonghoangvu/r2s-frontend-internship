c01c73d9fa9416adf6498a646f38607ebb7e555b
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-08-30 20:20:05 +0700

    chore: change date format for content date created

--- src/main/resources/static/js/add-content.js
+++ src/main/resources/static/js/add-content.js
@@ -22,11 +22,11 @@ function collectFormData() {
 }
 
 function formatDate(date) {
-	const year = date.getFullYear()
-	const month = date.getMonth() + 1
-	const day = date.getDate()
-	const hours = date.getHours()
-	const minutes = date.getMinutes()
+	const day = date.getDate().toString().padStart(2, '0')
+	const month = (date.getMonth() + 1).toString().padStart(2, '0')
+	const year = date.getFullYear().toString().padStart(4, '0')
+	const hours = date.getHours().toString().padStart(2, '0')
+	const minutes = date.getMinutes().toString().padStart(2, '0')
 	return `${day}/${month}/${year} ${hours}:${minutes}`
 }
 
