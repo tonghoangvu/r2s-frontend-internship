cec28667dcfd4a8b99bbd9b871cceeb685c73a0a
Tong Hoang Vu <tonghoangvu@outlook.com>
2021-09-08 16:53:25 +0700

    chore: remove unnecessary UserDetailsService registration

--- src/main/java/com/tonghoangvu/r2sfrontendinternship/config/WebSecurityConfig.java
+++ src/main/java/com/tonghoangvu/r2sfrontendinternship/config/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package com.tonghoangvu.r2sfrontendinternship.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,12 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	private final UserDetailsService userDetailService;
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailService);
-	}
+	private final UserDetailsService userDetailsService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -49,7 +43,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.deleteCookies("JSESSIONID")
 			.logoutSuccessUrl("/")
 			.and()
-			.rememberMe();
+			.rememberMe()
+			.userDetailsService(userDetailsService); // Required here
 	}
 
 	@Bean
